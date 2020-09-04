e2827f394a7b245b9b9c13485b60528032bccbca
Wallacewss2033 <wallace-2033@hotmail.com>
2020-09-04 13:30:51 -0300

    primeiro commit

---
+++ src/pages/TeacherForm/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PageHeader from '../../components/PageHeader/index';
+import './styles.css';
+
+
+function TeacherForm() {
+    return (
+        <div id="page-teacher-form" className="container">
+            <PageHeader title="Que incrivel que você quer da aulas.">
+              
+            </PageHeader>
+        </div>
+    )
+}
+
+export default TeacherForm;
