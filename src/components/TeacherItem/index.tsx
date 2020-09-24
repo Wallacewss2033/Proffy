85abeab18b91f120fb1a804774e34ce403065274
Wallacewss2033 <wallace-2033@hotmail.com>
2020-09-24 01:54:20 -0300

    teacher form stylizado

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -8,7 +8,7 @@ function TeacherItem() {
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://scontent.fssa2-2.fna.fbcdn.net/v/t31.0-8/20933769_1413983082052163_5640163231637311451_o.jpg?_nc_cat=106&_nc_sid=09cbfe&_nc_ohc=kuVp0Zl_eL4AX9blY_S&_nc_ht=scontent.fssa2-2.fna&oh=0c37c0ddee060ec6ba37d71470185ec8&oe=5F5EEFE1"
+                <img src="https://avatars0.githubusercontent.com/u/39920409?s=460&u=e0379153f42021f1edf2d8bd312507228ff077b8&v=4"
                     alt="Wallace Souza" />
                 <div>
                     <strong>Wallace Souza</strong>
--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,14 +1,42 @@
 import React from 'react';
+import Input from '../../components/Input';
 import PageHeader from '../../components/PageHeader/index';
+import WarningIcon from '../../assets/images/icons/warning.svg';
 import './styles.css';
 
 
 function TeacherForm() {
     return (
         <div id="page-teacher-form" className="container">
-            <PageHeader title="Que incrivel que você quer da aulas.">
-              
+            <PageHeader
+                title="Que incrivel que você quer da aulas."
+                description="O primeiro passo é preencher o fomulário de inscrição">
+
             </PageHeader>
+            <main>
+                <fieldset>
+                    <legend>Seus dados</legend>
+                    <Input name="name" label="Nome Completo"/>
+                    <Input name="avatar" label="Avatar"/>
+                    <Input name="whatsapp" label="WhatsApp"/>
+                </fieldset>
+                <fieldset>
+                    <legend>Sobre a aula</legend>
+                    <Input name="subject" label="Matéria"/>
+                    <Input name="cost" label="Custa da sua aula por hora"/>
+                    <Input name="whatsapp" label="WhatsApp"/>
+                </fieldset>
+                <footer>
+                    <p>
+                        <img src={WarningIcon} alt="Aviso importante"/>
+                        importante! <br/>
+                        preencha todos os dados
+                    </p>
+                    <button type="button">
+                        Salvar Cadastro
+                    </button>
+                </footer>
+            </main>
         </div>
     )
 }
